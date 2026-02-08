5182babb99c8cb4e6bf33f832bbe15557f29d5cb
prathamesh <prathameshsawant263@gmail.com>
2026-02-08 16:45:38 +0530

    Add custom exception classes for better error handling

---
+++ src/main/java/com/tinyguard/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+
+public class ResourceNotFoundException extends RuntimeException {
+
+public ResourceNotFoundException(String message) {
+public class ResourceNotFoundException extends RuntimeException {
+
+
+}
+}
